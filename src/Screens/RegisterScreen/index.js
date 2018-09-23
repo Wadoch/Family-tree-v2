a751a9c382d5d554cf3400ed930c9db326cdf9af
Wadoch <dwadoch@gmail.com>
2018-09-23 11:45:59 +0200

    Add back link in register page

--- src/Screens/RegisterScreen/index.js
+++ src/Screens/RegisterScreen/index.js
@@ -1,2 +1,24 @@
-import RegisterScreen from './RegisterScreen';
+import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+
+import RegisterContainer from '../../Containers/RegisterContainer';
+
+import styles from './styles/style.scss';
+
+class RegisterScreen extends Component {
+    render() {
+        return (
+            <div className={ styles.container }>
+                <div className={ styles.box }>
+                    <h2 className={ styles.title }>Register</h2>
+                    <RegisterContainer />
+                    <div>
+                        <Link to='/login'>Back to login</Link>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 export default RegisterScreen;
