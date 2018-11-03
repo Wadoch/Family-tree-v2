57527dbebf59822839dfb91f96ea2e58f3ae8248
Wadoch <dwadoch@gmail.com>
2018-11-03 12:57:34 +0100

    Add BFF url to prod version

---
+++ infrastructure/herokuConfig.js
@@ -0,0 +1,19 @@
+const heroin = require('heroin-js');
+
+const configurator = heroin(process.env.HEROKU_API_TOKEN);
+
+const copyEnvs = [
+    'REACT_APP_BFF_URL',
+];
+
+const baseEnvs = copyEnvs.reduce((all, current) =>
+    ({ ...all, [current]: process.env[current] }), {});
+
+const config = {
+    name: 'ps-family-tree',
+    config_vars: {
+        ...baseEnvs,
+    },
+};
+
+configurator(config);
